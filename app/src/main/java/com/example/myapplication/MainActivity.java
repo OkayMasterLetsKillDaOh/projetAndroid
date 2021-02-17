53747763ecb7d7efa49846937ce6bb42ddc2e0e6
MOMO Adrien Nickson <mani@ekwali.local>
2021-02-17 04:09:44 +0100

    Scéance du 17 Février

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,13 +2,32 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.myapplication.TP.TpMainActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button tpButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tpButton = (Button) (findViewById(R.id.tp_button));
+        System.out.println(tpButton);
+        tpButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent tpLayoutActivityIntent = new Intent(MainActivity.this, TpMainActivity.class);
+                startActivity(tpLayoutActivityIntent);
+            }
+        });
+
+
     }
 }
