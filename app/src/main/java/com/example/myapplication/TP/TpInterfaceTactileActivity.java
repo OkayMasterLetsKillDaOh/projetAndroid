a088b3baff1a5b2214857da66bca071c2a9a6f83
thomas <thomas.gilbert0316@gmail.com>
2021-02-28 11:29:23 +0100

    commit

--- app/src/main/java/com/example/myapplication/TP/TpInterfaceTactileActivity.java
+++ app/src/main/java/com/example/myapplication/TP/TpInterfaceTactileActivity.java
@@ -5,13 +5,28 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.myapplication.R;
+import com.example.myapplication.TP.customView.TpInterfaceTactileUnView;
 
 public class TpInterfaceTactileActivity extends AppCompatActivity {
 
+    TpInterfaceTactileUnView tpInterfaceTactileUnView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tp_interface_tactile);
+        tpInterfaceTactileUnView = findViewById(R.id.tp_interface_tactile_un);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        tpInterfaceTactileUnView.pause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        tpInterfaceTactileUnView.resume();
     }
 
 }
