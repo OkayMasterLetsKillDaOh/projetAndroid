db8ff825288b51b2e4b2e8ec39efc70bedebe938
MOMO Adrien Nickson <mani@ekwali.local>
2021-03-21 07:04:05 +0100

    Réticule

--- app/src/main/java/com/example/myapplication/TP/customView/CercleView.java
+++ app/src/main/java/com/example/myapplication/TP/customView/CercleView.java
@@ -1,21 +1,28 @@
 package com.example.myapplication.TP.customView;
 
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.View;
 
 import androidx.annotation.Nullable;
+
+import java.util.Random;
 
 /**
  * Created by MOMO Adrien Nickson on 19/02/21.
  */
 public class CercleView extends View {
 
-    private int canvasWidth;
-    private int canvasHeight;
+    private Canvas mExtraCanvas;
+    private Bitmap mExtraBitmap;
+    private float reticuleX;
+    private float reticuleY;
+    float rayon;
 
     public CercleView(Context context) {
         super(context);
@@ -36,20 +43,69 @@ public class CercleView extends View {
     @Override
     protected void onSizeChanged(int w, int h, int oldw, int oldh) {
         super.onSizeChanged(w, h, oldw, oldh);
-        canvasWidth = w;
-        canvasHeight = h;
+        mExtraBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
+        mExtraCanvas = new Canvas(mExtraBitmap);
+        if (reticuleX == 0.0f)
+            reticuleX = w/2;
+        if (reticuleY == 0.0f)
+            reticuleY = h/2;
+        rayon = Math.min( (float)(w*0.8), (float)(h*0.8))/8;
+        dessinerLeCercle();
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
-        Paint mPaint = new Paint();
-        canvas.drawColor(Color.RED);
-
-        mPaint.setColor(Color.BLUE);
-        canvas.drawCircle(canvasWidth/2, canvasHeight/2, Math.min( (float)(canvasWidth*0.8), (float)(canvasHeight*0.8))/2, mPaint);
-        mPaint.setColor(Color.BLACK);
-        canvas.drawRect(0, canvasHeight/3, canvasWidth, 2*canvasHeight/3, mPaint);
-        
         super.onDraw(canvas);
+        canvas.drawBitmap(mExtraBitmap, 0, 0, null);
     }
+
+    public void dessinerLeCercle(){
+        Paint mPaint = new Paint();
+        mPaint.setColor(Color.BLUE);
+        mExtraCanvas.drawColor(Color.WHITE);
+        mExtraCanvas.drawCircle(reticuleX, reticuleY, rayon, mPaint);
+        invalidate();
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+
+        int index = (event.getAction() & MotionEvent.ACTION_POINTER_INDEX_MASK) >> MotionEvent.ACTION_POINTER_INDEX_SHIFT;
+
+        if (event.getAction() != MotionEvent.ACTION_MOVE){
+            System.out.println( "\n\n" + index + "  --  " + event.toString());
+        }
+
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_MOVE:
+                int historySize = event.getHistorySize();
+                System.out.println(historySize);
+                if (historySize > 0){
+                    float dx = event.getX() - event.getHistoricalX(historySize-1);
+                    float dy = event.getY() - event.getHistoricalY(historySize-1);
+                    System.out.println("dx: " + dx + "; dy: " + dy);
+                    reticuleX+=dx;
+                    reticuleY+=dy;
+                    dessinerLeCercle();
+                }
+            default:
+                // do nothing
+        }
+
+        return true;
+    }
+
+    public void positionerReticule(float x, float y){
+        reticuleX = x;
+        reticuleY = y;
+    }
+
+    public float getReticuleX(){
+        return reticuleX;
+    }
+
+    public float getReticuleY(){
+        return reticuleY;
+    }
+
 }
--- app/src/main/java/com/example/myapplication/TP/interfaceTactile/TpInterfaceTactileDemoActivity.java
+++ app/src/main/java/com/example/myapplication/TP/interfaceTactile/TpInterfaceTactileDemoActivity.java
@@ -2,15 +2,33 @@ package com.example.myapplication.TP.interfaceTactile;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 
 import com.example.myapplication.R;
+import com.example.myapplication.TP.customView.CercleView;
 
 public class TpInterfaceTactileDemoActivity extends AppCompatActivity {
 
+    CercleView c;
+    SharedPreferences prefs;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tp_interface_tactile_demo);
+        c = findViewById(R.id.cercleView);
+
+        prefs = PreferenceManager.getDefaultSharedPreferences(this); //getPreferences(MODE_PRIVATE)
+        if (prefs.contains("reticule_x") && prefs.contains("reticule_y")){
+            c.positionerReticule(prefs.getFloat("reticule_x", -1f), prefs.getFloat("reticule_y", -1f));
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        prefs.edit().putFloat("reticule_x", c.getReticuleX()).apply();
+        prefs.edit().putFloat("reticule_y", c.getReticuleY()).apply();
     }
 }
